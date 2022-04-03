9f4ece178a3e0416d92e714c17ebb0bb5c4e2f87
abbiyudha <abbiyudha14@gmail.com>
2022-04-03 23:42:55 +0700

    user

--- usecase/user/interace.go
+++ usecase/user/interace.go
@@ -1,1 +1,11 @@
 package user
+
+import "todoListApp/entities"
+
+type UserUseCaseInterface interface {
+	GetAll() ([]entities.User, error)
+	GetUserById(id int) (entities.User, error)
+	CreateUser(user entities.User) error
+	DeleteUser(id int) error
+	UpdateUser(id int, user entities.User) error
+}
